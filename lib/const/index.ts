4353fabc91a31344017a4aefb24f1f40cd9862d0
WangBinyu <406279919@qq.com>
2020-03-19 13:13:55 +0800

    update tree's format

---
+++ lib/const/index.ts
@@ -0,0 +1,5 @@
+export const CHILDREN = 'children'
+export const CHILD = 'child'
+export const PARENT = 'parent'
+export const ID = 'id'
+export const PID = 'pid'
--- lib/object/tree.ts
+++ lib/object/tree.ts
@@ -1,27 +1,55 @@
-export function toTree<T> (
-  option: { idField: string, pidField: string, childrenField: string, parentField: string}, 
-  arr: Array<T> ) {
-  
-  const defaultOpt = { idField: 'id', pidField: 'pid', childrenField: 'children', parentField: 'parent' }
-  const opt = Object.assign(defaultOpt, option)
 
-  const nodeMap = {}
+import { curry } from '../operator'
+import { ID, PID, CHILDREN, PARENT } from '../const'
+
+function _toTree<T = any, K = T> (
+  option: { 
+    idField?: string, 
+    pidField?: string, 
+    childrenField?: string, 
+    parentField?: string, 
+    childValid?: (parent: T, childLike: T) => boolean 
+  }, 
+  arr: Array<T> 
+) {
+  
+  const defaultOpt = { 
+    idField: ID, 
+    pidField: PID, 
+    childrenField: CHILDREN, 
+    parentField: PARENT,
+    childValid: (parentLike: T, child: T) => parentLike[opt.idField] === child[opt.pidField]
+  }
+  const opt = Object.assign(defaultOpt, option)
+  const result:K[] = []
 
   arr.forEach(function format (node) {
-    
+    const parent = arr.find(item => opt.childValid(item, node))
+
+    if (parent) {
+      node[opt.parentField] = parent
+      if (!Array.isArray(parent[opt.childrenField])) {
+        parent[opt.childrenField] = []
+      }
+      parent[opt.childrenField].push(node)
+    }
+    else {
+      result.push(node as any)
+    }
   })
   
-  console.warn('toTree 为实现')
+  return result
 
 }
 
-export function flatTree<T = any> (
-  option: { childrenField: string, childValid: (n: T) => boolean }, 
-  node: T
+function _flattenTree<T = any> (
+  option: { childrenField?: string, childValid?: (n: T) => boolean }, 
+  node: T | T[]
 ) {
 
+  const nodes = Array.isArray(node) ? node : [node]
   let result: T[] = []
-  const defaultOpt = { childrenField: 'children', childValid: (n:T) => n[opt.childrenField].length > 0 }
+  const defaultOpt = { childrenField: CHILDREN, childValid: (n:T) => n[opt.childrenField].length > 0 }
   const opt = Object.assign(defaultOpt, option)
 
   function flat (n: T) {
@@ -35,7 +63,10 @@ export function flatTree<T = any> (
     }
   }
 
-  flat(node)
+  nodes.forEach(flat)
 
   return result
 }
+
+export const toTree = curry(_toTree)
+export const flattenTree = curry(_flattenTree)
